7a246875505a70d2ff0f2c0b562569de67929868
DanielGalandak <daniel.galandak@gmail.com>
2024-11-24 17:30:34 +0100

    odevzdani ukolu 05_dates

---
+++ 05/05_dates.py
@@ -0,0 +1,15 @@
+# '05_dates.txt'
+
+import datetime
+
+PATH = '05/05_dates.txt'
+
+with open(PATH, mode='w', encoding='utf-8') as file:
+
+    for i in range(1, 13):
+        d = datetime.date(2024, i, 1)   # vytvoření data pomocí hodnoty z 'range'
+        first_day = d.strftime('%d/%m/%Y')          
+        file.write(f'{first_day}\n')
+        print(first_day)
+
+
